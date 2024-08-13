6c52c0da1c48a7b733f53275e0d8c6256499929c
tigocode <tiago.dantas16@live.com>
2024-08-13 00:53:03 -0300

    terceira versao: construcao da ultima etapa do projeto

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,18 @@
-import styles from "./page.module.css";
+'use client';
+import { useState } from 'react';
+import styles from './page.module.css';
 
-export default function Home() {
+export default function Home(props) {
   return (
     <main className={styles.container}>
-     <section className={styles.container_banner}>
-     </section>
-     <section>
-     </section>
+      <section className={styles.container_banner}>
+      </section>
+      <section className={styles.container_text}>
+        <h1>Bem-vindo a Barber Shop</h1>
+        <p className={styles.text}>Nossa barbearia sempre oferece profissionais de qualidade e estamos prontos para lidar com suas maiores expectativas.</p>
+        <p className={styles.text}>Nossos serviços são dedicados ao seu sucesso pessoal. Aqui temos uma equipe premiada que demonstrou o talento de mestres barbeiros em vários concursos de estilo. Deixe nosso barbeiro ser seu estilista pessoal e você nunca ficará desapontado.</p>
+        <p className={styles.assinatura}>S. Kelly</p>
+      </section>
     </main>
   );
 }
--- src/components/Topo/index.jsx
+++ src/components/Topo/index.jsx
@@ -1,3 +1,5 @@
+'use client';
+import { useState } from 'react';
 import Image from 'next/image';
 import { BsSun, BsMoon } from 'react-icons/bs';
 
@@ -5,11 +7,19 @@ import styles from './Topo.module.css';
 import Logo from '../../../public/barbearia.png';
 
 export default function Topo() {
+  const [ ehThemeDark, setEhThemeDark ] = useState(false);
+
+  console.log(ehThemeDark);
+
+  function toChangeTheme() {
+    setEhThemeDark(!ehThemeDark);
+    console.log(ehThemeDark);
+  }
   return(
     <header className={styles.container}>
       <div className={styles.container_topo}>
         <Image src={Logo} className={styles.image} />
-        <button>
+        <button onClick={toChangeTheme}>
           <BsSun className={styles.icon} />
         </button>
       </div>
